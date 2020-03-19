edd936c8da4e218af79f9d9701fd8d16c01bcfb7
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-19 05:56:32 -0500

    alternate keyboards with shift key press

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // hide uppercase keyboard when page loads
-    $('#keyboard-upper-container').hide()
+    $('#keyboard-upper-container').hide();
 
-
+    // determine if shift key was pressed
+    $(document).on({
+        keydown: function(e) {
+          if (e.originalEvent.key === "Shift") {
+            $('#keyboard-upper-container').toggle();
+            $('#keyboard-lower-container').toggle();
+          }
+        }
+    });
+    $(document).on({
+        keyup: function(e) {
+            if (e.originalEvent.key === "Shift") {
+              $('#keyboard-upper-container').toggle();
+              $('#keyboard-lower-container').toggle();
+            }
+        }
+    })
 });
